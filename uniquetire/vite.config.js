4cdcb1f97a35172443747b642eb35095ce1cf7c5
Chit <ckko@bsu.edu>
2025-01-01 22:57:02 -0500

    add dist

--- uniquetire/vite.config.js
+++ uniquetire/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Output directory
+  },
   plugins: [vue()], // Correctly closed the plugins array
   resolve: {
     alias: {
